dad70e8d8e173ceb08655340a2a952c01643c2d1
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2017-09-14 20:09:02 -0700

    Basic get routes set

--- server.js
+++ server.js
@@ -10,7 +10,33 @@ var fs = require("fs");
 var app = express();
 var PORT = 3000;
 
+var waitingList = [];
 
+var currentTables = [{
+  "customerName": "",
+  "phoneNumber": "",
+  "customerEmail": "",
+  "customerID": "<iframe src=\"https://giphy.com/embed/13HgwGsXF0aiGY\" width=\"480\" height=\"270\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe><p><a href=\"https://giphy.com/gifs/13HgwGsXF0aiGY\"></a></p>"
+  },
+  {
+  "customerName": "",
+  "phoneNumber": "",
+  "customerEmail": "",
+  "customerID": "<iframe src=\"https://giphy.com/embed/yYSSBtDgbbRzq\" width=\"480\" height=\"360\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe><p><a href=\"https://giphy.com/gifs/frustrated-annoyed-programming-yYSSBtDgbbRzq\"></a></p>"
+  },
+  {
+  "customerName": "",
+  "phoneNumber": "",
+  "customerEmail": "",
+  "customerID": "<iframe src=\"https://giphy.com/embed/zOvBKUUEERdNm\" width=\"480\" height=\"270\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe><p><a href=\"https://giphy.com/gifs/coding-zOvBKUUEERdNm\"></a></p>"
+  },
+  {
+  "customerName": "",
+  "phoneNumber": "",
+  "customerEmail": "",
+  "customerID": "Us right now, lolz"
+  }
+];
 
 // / --> landing page
 // /tables
@@ -23,4 +49,39 @@ var PORT = 3000;
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname,"index.html"));
-})
+});
+app.get("/reservation", (req, res) => {
+	res.sendFile(path.join(__dirname,"reservation.html"));
+});
+
+app.get("/api/tables", (req, res) => {
+    res.send(currentTables);
+});
+
+app.get("/api/waitlist", (req, res) => {
+    res.send(waitingList);
+});
+
+app.get("/api/clear", (req, res) => {
+    waitingList = [];
+    currentTables = [];
+    res.send("Database cleared!")
+});
+
+app.post("/api/tables", (req, res) => {
+  var data = req.body;
+
+  if(currentList.length >= 5){
+    //push data to wishList
+    waitingList.push(data);
+  } else {
+    //push to currentList
+    currentList.push(data);
+  }
+
+});
+
+
+app.listen(PORT, function(){
+  console.log("RUnning on port " + PORT);
+});
